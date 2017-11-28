2e819a5b70ef5de59459f027a39b03e9e6d7beee
TMad <madsen.tyler@gmail.com>
2017-11-28 11:57:18 -0700

    add average sales and year/sales validation

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,8 +1,15 @@
 class Film < ActiveRecord::Base
 
-  validates :title, presence: true
+  validates :title, uniqueness: true
+  # validates :title, presence: true
+  validates_presence_of :title, :year, :box_office_sales
+  # validates_uniqueness_of :title, :year
 
   def self.total_box_office_sales
     sum(:box_office_sales)
+  end
+
+  def self.average_box_office_sales
+    average(:box_office_sales)
   end
 end
--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -8,11 +8,29 @@ RSpec.describe Film do
         expect(Film.total_box_office_sales).to eq(7)
       end
     end
+    describe ".average_box_office_sales" do
+      it "returns average box office sales" do
+        Film.create(title: "Fargo", year: 2017, box_office_sales: 3)
+        Film.create(title: "Die Hard", year: 2016, box_office_sales: 5)
+
+        expect(Film.average_box_office_sales).to eq(4)
+      end
+    end
   end
 
   describe "Validations" do
     it "is invalid without a title" do
       film = Film.new(year: 2017, box_office_sales: 2)
+
+      expect(film).to be_invalid
+    end
+    it "is invalid without year" do
+      film = Film.new(title: "Drop Dead Fred", box_office_sales: 5)
+
+      expect(film).to be_invalid
+    end
+    it "is invalid without box_office_sales" do
+      film = Film.new(title: "Drop Dead Fred", year: 1980)
 
       expect(film).to be_invalid
     end
